18ff2452aef689ec69901348eeb660c5d5310673
RiyanBK <rkadribegovic1@hwemail.com>
2024-10-13 20:37:38 -0700

    migrated everything to Git.java and implemented stage for GitInterface

--- Git.java
+++ Git.java
@@ -10,8 +10,151 @@ import java.security.DigestException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.zip.*;
+import java.util.Calendar;
 
-public class Git {
+public class Git implements GitInterface {
+    private String currentDate;
+    private String headHash;
+    private String treeHash;
+    private String hashOfCommit;
+
+    public void stage(String filePath) {
+        try {
+            Git.createBlob(Paths.get(filePath), false);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String commit(String author, String message) {
+        currentDate = getDate();
+        findHeadHash();
+        if (!headHash.equals("")) {
+            // add extra files from head into index
+            writeToIndex();
+        }
+        getTreeContents();
+        StringBuffer toCommit = new StringBuffer("");
+        toCommit.append("tree: " + treeHash);
+        toCommit.append("\nparent: " + headHash);
+        toCommit.append("\nauthor: " + author);
+        toCommit.append("\ndate: " + currentDate);
+        toCommit.append("\nmessage: " + message);
+        String commitContent = toCommit.toString();
+        try {
+            // create a file to put the commit in
+            // make the commit file
+            // get that hash and put it in head
+
+            File tempCommitFile = File.createTempFile("contentToHash", null);
+            FileWriter writer = new FileWriter(tempCommitFile);
+            writer.write(commitContent);
+            writer.close();
+            hashOfCommit = Git.sha1(tempCommitFile.toPath());
+            writer = new FileWriter(new File("./git/HEAD"));
+            writer.write(hashOfCommit);
+            writer.close();
+            writer = new FileWriter(new File("./git/objects/" + hashOfCommit));
+            writer.write(commitContent);
+            writer.close();
+            // creates tree snapshot of the tree and puts in objects
+            BufferedReader reader = new BufferedReader(new FileReader("./git/index"));
+            StringBuffer sb = new StringBuffer();
+            while (reader.ready()) {
+                sb.append((char) reader.read());
+            }
+            reader.close();
+            writer = new FileWriter(new File("./git/objects/" + treeHash));
+            writer.write(sb.toString());
+            writer.close();
+            // overwrites index once commit is created
+            writer = new FileWriter(new File("./git/index"));
+            writer.write("");
+            writer.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return hashOfCommit;
+    }
+
+    public void checkout(String commitHash) {
+
+    }
+
+    public void findHeadHash() {
+        StringBuffer headHashReader = new StringBuffer("");
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(new File("./git/HEAD")));
+            while (reader.ready()) {
+                headHashReader.append(((char) reader.read()));
+            }
+            reader.close();
+            headHash = headHashReader.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void writeToIndex() {
+        // part 1 finds the hash of the previous tree
+        // currently we have the hash of the commit
+        String oldTreeHash = "";
+        try {
+            // this finds the commit
+            BufferedReader reader = new BufferedReader(new FileReader("./git/objects/" + headHash));
+            // read just after the first 6 characters of the tree:
+            // ("tree: " + hash of tree)
+            // we just want the hash of the tree
+            oldTreeHash = reader.readLine();
+            oldTreeHash = oldTreeHash.substring(6);
+            reader.close();
+
+            if (oldTreeHash.equals("")) {
+                return;
+            }
+
+            // part 2 actually writes to the file
+            reader = new BufferedReader(new FileReader("./git/objects/" + oldTreeHash));
+            while (reader.ready()) { // read index list of previous tree
+                String nextBlob = reader.readLine().substring(46);
+                if (!nextBlob.contains("/")) { // really jank technical way of doing this but it works!
+                    Git.createBlob(Paths.get(nextBlob), false);
+                }
+                // add all files not already listed in index to index
+            }
+            reader.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void getTreeContents() {
+        // StringBuffer content = new StringBuffer("");
+        // try {
+        // BufferedReader reader = new BufferedReader(new FileReader(new
+        // File("./git/index")));
+        // while (reader.ready()) {
+        // content.append((char) reader.read());
+        // }
+        // reader.close();
+        // } catch (Exception e) {
+        // e.printStackTrace();
+        // }
+        try {
+            treeHash = Git.sha1(Paths.get("./git/index"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getDate() {
+        Calendar date = Calendar.getInstance();
+        int day = date.get(Calendar.DAY_OF_MONTH);
+        int month = date.get(Calendar.MONTH);
+        int year = date.get(Calendar.YEAR);
+        return ((month + 1) + "/" + day + "/" + year);
+    }
     public static void main(String[] args) throws DigestException, NoSuchAlgorithmException, IOException {
         // Test creating repository when it doesn't exist (should print "Initialized
         // repository and deleted files")
@@ -26,6 +169,13 @@ public class Git {
         // Path path = Paths.get("/Users/oliviakong/Desktop/everything
         // basically/forkedcodetest/newFolder");
         // createBlob(path, false);
+    }
+
+    public Git () {
+        try {
+        initRepo(); } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     // Tests initRepo() for when directory already exists or doesn't exist yet
--- GitTester.java
+++ GitTester.java
@@ -8,22 +8,18 @@ public class GitTester {
         try {
             // part 1: create initial commit
             Git.deleteEverything(Paths.get("git"));
-            try {
-                // Testing git creation and repo initialization:
-                File testFile = new File("testFile.txt");
-                File testDir = new File("testDir");
-                File testFile2 = new File("testDir/testFile2.txt");
-                testFile.createNewFile();
-                testDir.mkdir();
-                testFile2.createNewFile();
-                Git.initRepo();
-                Git.createBlob(Paths.get("testFile.txt"), false);
-                Git.createBlob(Paths.get("testDir"), false);
-                // do the commit
-                Commit commitTestPart1 = new Commit("Riyan", "part 1");
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+            // Testing git creation and repo initialization:
+            File testFile = new File("testFile.txt");
+            File testDir = new File("testDir");
+            File testFile2 = new File("testDir/testFile2.txt");
+            testFile.createNewFile();
+            testDir.mkdir();
+            testFile2.createNewFile();
+            // do the commit
+            Git dir = new Git();
+            dir.stage("testFile.txt");
+            dir.stage("testDir");
+            dir.commit("Riyan", "part 1");
 
             // part 2: create second commit
             File newTestFile = new File("newTestFile.txt");
@@ -31,9 +27,9 @@ public class GitTester {
             BufferedWriter writer = new BufferedWriter(new FileWriter("newTestFile.txt"));
             writer.write("insert test content here");
             writer.close();
-            Git.createBlob(Paths.get("newTestFile.txt"), false);
             // do the commit
-            Commit commitTestPart2 = new Commit("Riyan", "part 2");
+            dir.stage("newTestFile.txt");
+            dir.commit("Riyan", "part 2");
         } catch (Exception e) {
             e.printStackTrace();
         }
